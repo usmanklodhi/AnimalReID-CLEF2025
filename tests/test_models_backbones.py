25e873ff2e68039d201c20db1f3ed96a03c2e3fb
Usman Khan Lodhi <usmanklodhii@gmail.com>
2025-06-29 16:05:09 +0200

    skipping heavy model testing in ci, correcting input size for models

--- tests/test_models_backbones.py
+++ tests/test_models_backbones.py
@@ -1,5 +1,7 @@
 import torch
 from src.models.backbones import MultiBackboneClassifier, EnsembleNet
+import os
+import pytest
 
 def test_multi_backbone_classifier_forward():
     # Use small dummy models for test
@@ -12,8 +14,10 @@ def test_multi_backbone_classifier_forward():
     assert out.shape == (4, num_classes)
 
 def test_ensemble_net_forward():
+    if os.getenv("CI"):
+        pytest.skip("Skipping heavy model test in CI/CD")
     num_classes = 2
     model = EnsembleNet(num_classes)
-    x = torch.randn(4, 3, 224, 224)
+    x = torch.randn(4, 3, 384, 384)
     out = model(x)
     assert out.shape == (4, num_classes) 
